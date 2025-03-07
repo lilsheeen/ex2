e0c962d5e1e720ad7a98ab0b9e34fca7b9cbc074
lilsheeen <lilsheen3@gmail.com>
2025-03-07 14:42:01 +0330

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,8 @@
+name=input("please enter your name:")
+age=input("please enter your age:")
+height=input("please enter your height in centimeters:")
+#show information
+print("your information")
+print("name:","Shaghayegh")
+print("age:",21)
+print("height:",165, "cm")
